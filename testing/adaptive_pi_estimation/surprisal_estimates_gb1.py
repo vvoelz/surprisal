3db3dd98c213433ceb816b1eaf0d090c19d9ade0
vvoelz <vvoelz@gmail.com>
2016-02-13 15:33:38 -0500

    works with new surprisal library; fixed log(0) bug

--- testing/adaptive_pi_estimation/surprisal_estimates_gb1.py
+++ testing/adaptive_pi_estimation/surprisal_estimates_gb1.py
@@ -9,70 +9,6 @@ from scipy import io
 from scipy.sparse import issparse
 
 
-###### Functions #######
-
-def estimate_pi_i(C, pi_i = None, ftol=1e-12, maxiters=100000):
-    """
-    Use the self-consistent TRAM estimator of Noe et al to reversibly estimate the equilibrium distribution.
-
-    	pi_i = \sum_j  (C_ij + C_ji)/(C_i/pi_i^(k) + C_j/pi_j)
-
-    Iteration will continue until either the maximum number is reached, or the reduced free energies
-    reach a tolerance threshold
-
-    Inputs
-    ------
-    C          - array of observed counts C_ij from state i to j
-
-    Parameters
-    ---------
-    pi_i       - an array of initial guesses for the equil distribution
-    ftol       - tolerance of the maximum difference in log-populations (free energies)
-    maxiters   - mamimum number of self-consistent iterations
-
-    Returns
-    -------
-    pi_i       - the TRAM estimate of the equilibrium distribution
-
-    """
-
-
-    nstates = C.shape[0]
-    C_i = C.sum(axis=1)
-
-    # If an initial guess is not supplied, use a uniform distribution
-    if not ('numpy' in str(type(pi_i))):
-        pi_i = np.ones( nstates, dtype='float64')/float(nstates)
-    else:
-        pi_i = pi_i.astype('float64')
-
-    # helper quantity:  matrix of elements a_ij = C_ij + C_ji
-    a = C + C.transpose() 
-
-    pi_i_next = np.copy(pi_i)
-    for trial in xrange(maxiters):
-
-        # helper quantity: a matrix of elements b_ij = (\sum_j C_ij)/pi_i + (\sum_i C_ij)/pi_j
-        x = C_i/pi_i
-        b = np.tile(x.reshape(nstates,1), nstates) + np.tile(x, (nstates,1))
-
-        pi_i_next = (a/b).sum(axis=1)
-
-        f_i, f_i_next = -np.log(pi_i), -np.log(pi_i_next)
-        # compute the absolute change of f_i
-        df = np.max(np.abs(f_i - f_i_next))
-        if trial%100 == 0:
-            print '### estimator iter', trial, 'df', df, 'pi_i.sum()', pi_i.sum() #, 'pi_i_next', pi_i_next
-        
-        pi_i = np.copy(pi_i_next)
-        pi_i = pi_i/pi_i.sum()  # make sure populations is normalized
-     
-        if df < ftol:
-            return pi_i
-
-    print 'WARNING: maxiters=%d reached; not converged to ftol = %e'%(maxiters, ftol)
-    return pi_i
-
 
 # Load in the GB1 hairpin macrostate transition matrix
 T = io.mmread('./gb1-tProb.mtx')
@@ -128,7 +64,7 @@ for trial in range(len(nsamples)):
             C[i, jump_indices[j]] += sampled[j]
             C_star[i, jump_indices[j]] += sampled_star[j]
 
-    # update counts and pi_i
+    # update counts (C and C_star) and equil pops (pi_i and pi_i_star)
     sa.update([C, C_star]) 
 
     total_samples = nsamples[0:trial+1].sum()
@@ -154,8 +90,9 @@ for trial in range(len(nsamples)):
         print i, total_samples, var_analytic[-1], var_bootstrap[-1], analytic_time, bootstrap_time
 
     var_analytic, var_bootstrap = np.array(var_analytic), np.array(var_bootstrap)
-    print 'Corrcoef of log(var) =', np.corrcoef(np.log(var_analytic[var_analytic>0]), np.log(var_bootstrap[var_bootstrap>0]))
-    print 'Average std(log10(var))  =', np.mean( (np.log10(var_analytic)-np.log10(var_bootstrap))**2.0 )**0.5
+    Ind = (var_analytic > 0.)*(var_bootstrap > 0.) # avoid log(0) in plot 
+    print 'Corrcoef of log(var) =', np.corrcoef(np.log(var_analytic[Ind]), np.log(var_bootstrap[Ind]))
+    print 'Average std(log10(var))  =', np.mean( (np.log10(var_analytic[Ind])-np.log10(var_bootstrap[Ind]))**2.0 )**0.5
 
     # plot analytical vs bootstrap estimates of surpirsal uncertinaty
     plt.subplot(1,2,2)
